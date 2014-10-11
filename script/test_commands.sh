b686532c47ac698c384b51b6f8719e858c745238
Mark Stratmann <mark@stratmann.me.uk>
2014-10-11 19:06:14 +0100

    Tweak sit map output url to include #

---
+++ script/test_commands.sh
@@ -0,0 +1,7 @@
+~/Downloads/s3cmd-master/s3cmd ls s3://logs-stockflare.com/cloudfront/EQXPEAUSXME2J.2014-10-05* | awk -F' ' '{if(NF>1){print $NF} else {print $NF}}' | xargs -L1 ~/Downloads/s3cmd-master/s3cmd get
+
+~/Downloads/s3cmd-master/s3cmd ls s3://logs-stockflare.com/cloudfront/*2014-10-07* | awk -F' ' '{if(NF>1){print $NF} else {print $NF}}' | xargs -L1 ~/Downloads/s3cmd-master/s3cmd get
+
+~/Downloads/s3cmd-master/s3cmd ls s3://logs-stockflare.com/cloudfront/ | grep .2014-10-07. | awk -F' ' '{if(NF>1){print $NF} else {print $NF}}' | xargs -L1 ~/Downloads/s3cmd-master/s3cmd get
+
+~/Downloads/s3cmd-master/s3cmd ls s3://logs-stockflare.com/s3/ | grep 2014-10-07 | awk -F' ' '{if(NF>1){print $NF} else {print $NF}}' | xargs -L1 ~/Downloads/s3cmd-master/s3cmd get
